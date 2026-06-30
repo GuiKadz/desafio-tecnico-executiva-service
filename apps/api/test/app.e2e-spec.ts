abfeb0be7d6e040a3b96e118f8e2b235d7389fe6
Guilherme Kadubitzki <kadubitzkig@gmail.com>
2026-06-30 05:24:06 -0300

    fix: corrige import do supertest (default import em vez de namespace)

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { randomUUID } from 'node:crypto';
 import { AppModule } from '../src/app.module';
 import { PrismaExceptionFilter } from '../src/common/filters/prisma-exception.filter';
